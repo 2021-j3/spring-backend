5b60fcbefc66e3f4928499f63aa49a8479f98aff
junsu hwang <junsu0325@naver.com>
2021-01-12 08:25:07 +0900

    init settings

---
+++ src/main/java/com/ecommerce/j3/J3Application.java
@@ -0,0 +1,13 @@
+package com.ecommerce.j3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/j3/J3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.j3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
