a6ccc778e73915fc36945b22d666857f1b7726b1
penguin418 <penguin418@naver.com>
2021-01-18 22:46:12 +0900

    feat: add AccountMapper for AccountDTO
    from now on, AccountService will use AccountMapper to convert between entity and DTO

--- src/main/java/com/ecommerce/j3/service/AccountService.java
+++ src/main/java/com/ecommerce/j3/service/AccountService.java
@@ -1,9 +1,11 @@
 package com.ecommerce.j3.service;
 
-import com.ecommerce.j3.controller.AccountDTO;
+import com.ecommerce.j3.domain.AccountDTO;
 import com.ecommerce.j3.domain.Account;
+import com.ecommerce.j3.domain.AccountMapper;
 import com.ecommerce.j3.domain.AccountType;
 import com.ecommerce.j3.repository.AccountRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -14,18 +16,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.Errors;
 
 import java.util.*;
 
 @Service
 @Transactional
+@AllArgsConstructor
 public class AccountService implements UserDetailsService {
-    private AccountRepository accountRepository;
-
-    @Autowired
-    public AccountService(AccountRepository accountRepository){this.accountRepository = accountRepository;}
-
+    private final AccountRepository accountRepository;
+    private final AccountMapper accountMapper;
 
     public Account save(Account account){
         accountRepository.save(account);
@@ -47,7 +46,7 @@ public class AccountService implements UserDetailsService {
     }
 
     public AccountDTO save(AccountDTO accountInfo){
-        Account account = accountInfo.toEntity(); // dto -> entity
+        Account account = accountMapper.toEntity(accountInfo); // dto -> entity
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // Bean 에 등록된 인코더불러오기
         account.setPasswordHash(passwordEncoder.encode(accountInfo.getPassword())); // 해시처리
         account.setAccountType(AccountType.USER); // 웹 페이지에서 처음 생성 시 무조건 user
@@ -59,23 +58,23 @@ public class AccountService implements UserDetailsService {
         Account accountFromDB = accountRepository
                 .findByEmail(accountInfo.getEmail())
                 .orElseThrow(()->new UsernameNotFoundException("DB에서 유저가 삭제됬습니다"));
-        // ... 이렇게 하나씩 바꾸다가 unique 옵션을 위배할 수도 있음
+        accountMapper.updateFromDTO(accountInfo, accountFromDB);
         accountRepository.save(accountFromDB);
-        return new AccountDTO(accountFromDB);
+        return accountMapper.toDTO(accountFromDB);
     }
 
     public AccountDTO findById(Long accountId){
         Account account = accountRepository
                 .findById(accountId)
                 .orElseThrow(()->new UsernameNotFoundException("다음 정보로 Account를 찾을 수 없습니다: accountId=" + accountId.toString()));
-        return new AccountDTO(account);
+        return accountMapper.toDTO(account);
     }
 
     public AccountDTO findByEmail(String email) {
         Account account = accountRepository
                 .findByEmail(email)
                 .orElseThrow(()->new UsernameNotFoundException("다음 정보로 Account를 찾을 수 없습니다: email=" + email));
-        return new AccountDTO(account);
+        return accountMapper.toDTO(account);
     }
 
     @Override
